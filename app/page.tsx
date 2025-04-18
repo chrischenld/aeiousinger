e2b29d54032d59255c2d2fa5e34ce269eef55463
chrischenld <christopherchen0@gmail.com>
2025-04-18 15:43:05 -0700

    fix: Resolves tailwind upgrade and extended color issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import Canvas from "@/components/sections/Canvas";
-import Header from "@/components/compositions/Header";
-
 export default function Home() {
 	return (
 		<div className="GridLayout h-screen overflow-hidden">
 			<main className="grid grid-cols-subgrid col-span-full h-screen">
-				<div className="grid grid-cols-subgrid grid-rows-[auto_1fr] col-span-18 h-screen"></div>
+				<div className="grid grid-cols-subgrid grid-rows-[auto_1fr] col-span-18 h-screen">
+					pee
+				</div>
 				<div className="grid grid-cols-subgrid col-span-6 h-screen">poo</div>
 			</main>
 		</div>
