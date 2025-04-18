ec400fe901764e63f0309e46c85ac363fdd30a63
chrischenld <christopherchen0@gmail.com>
2025-04-18 15:26:40 -0700

    fix: Re-adds tailwind config and postcss config after tailwind 4 upgrade

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,7 @@ export default function Home() {
 	return (
 		<div className="GridLayout h-screen overflow-hidden">
 			<main className="grid grid-cols-subgrid col-span-full h-screen">
-				<div className="grid grid-cols-subgrid grid-rows-[auto_1fr] col-span-18 h-screen">
-					<Header />
-					<Canvas />
-				</div>
+				<div className="grid grid-cols-subgrid grid-rows-[auto_1fr] col-span-18 h-screen"></div>
 				<div className="grid grid-cols-subgrid col-span-6 h-screen">poo</div>
 			</main>
 		</div>
